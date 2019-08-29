5de9bb211363a8ab741cd1420a0a511d716253f7
Chen Weiguang <chen.weiguang@gmail.com>
2019-08-29 21:49:49 +0800

    Add shellcheck and fix push script

--- push-images.sh
+++ push-images.sh
@@ -8,4 +8,4 @@ PYSPARK_TAG_SUFFIX="$(if [ "${WITH_PYSPARK}" = "true" ]; then echo _pyspark; fi)
 TAG_NAME="${SPARK_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}_${HIVE_TAG_SUFFIX}${PYSPARK_TAG_SUFFIX}_${DIST}"
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
-docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}
+docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}"
