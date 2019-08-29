fa2cb49d485ae7c86a31a59a78ad0127ab6a832b
Chen Weiguang <chen.weiguang@gmail.com>
2019-08-29 21:35:39 +0800

    Fix push image script

--- push-images.sh
+++ push-images.sh
@@ -3,13 +3,9 @@ set -euo pipefail
 
 IMAGE_NAME=${IMAGE_NAME:-spark-custom-addons}
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    ORIGINAL_IMAGE="${DOCKER_USERNAME}/${IMAGE_NAME}:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
-
-    IMAGE_NAME="test-${IMAGE_NAME}"
-    TEST_IMAGE="${DOCKER_USERNAME}/${IMAGE_NAME}:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
-    docker tag "${ORIGINAL_IMAGE}" "${TEST_IMAGE}"
-fi
+HIVE_TAG_SUFFIX="$(if [ "${WITH_HIVE}" = "true" ]; then echo _hive; fi)"
+PYSPARK_TAG_SUFFIX="$(if [ "${WITH_PYSPARK}" = "true" ]; then echo _pyspark; fi)"
+TAG_NAME="${SPARK_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}_${HIVE_TAG_SUFFIX}${PYSPARK_TAG_SUFFIX}_${DIST}"
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
-docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
+docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}
