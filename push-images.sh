f16fc6a3b704df3c9a91add2a1edd2152f38f819
Chen Weiguang <chen.weiguang@gmail.com>
2019-09-09 14:29:08 +0800

    Use scala version dependent images

--- push-images.sh
+++ push-images.sh
@@ -5,7 +5,7 @@ IMAGE_NAME=${IMAGE_NAME:-spark-custom-addons}
 
 HIVE_TAG_SUFFIX="$(if [ "${WITH_HIVE}" = "true" ]; then echo _hive; fi)"
 PYSPARK_TAG_SUFFIX="$(if [ "${WITH_PYSPARK}" = "true" ]; then echo _pyspark; fi)"
-TAG_NAME="${SPARK_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}${HIVE_TAG_SUFFIX}${PYSPARK_TAG_SUFFIX}_${DIST}"
+TAG_NAME="${SPARK_VERSION}_scala-${SCALA_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}${HIVE_TAG_SUFFIX}${PYSPARK_TAG_SUFFIX}_${DIST}"
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}"
