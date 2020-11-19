28ffc7c28870327195792e1f6f476aee9867a0c8
Weiguang <chen.weiguang@gmail.com>
2020-11-19 09:58:02 +0800

    chore(cleanup): commonize global env vars (#33)

--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+echo "${DOCKER_PASSWORD}" | docker login -u="${DOCKER_USERNAME}" --password-stdin
 
-IMAGE_NAME=${IMAGE_NAME:-spark-custom-addons}
-
-TAG_NAME="${SELF_VERSION}_${SPARK_VERSION}_scala-${SCALA_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}${HIVE_TAG_SUFFIX}${PYSPARK_TAG_SUFFIX}_${DIST}"
 docker tag "${IMAGE_NAME}:${TAG_NAME}" "${IMAGE_ORG}/${IMAGE_NAME}:${TAG_NAME}"
 docker push "${IMAGE_ORG}/${IMAGE_NAME}:${TAG_NAME}"
-
-ALT_TAG_NAME="${SPARK_VERSION}_scala-${SCALA_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}${HIVE_TAG_SUFFIX}${PYSPARK_TAG_SUFFIX}_${DIST}"
-docker tag "${IMAGE_NAME}:${TAG_NAME}" "${IMAGE_ORG}/${IMAGE_NAME}:${ALT_TAG_NAME}"
-docker push "${IMAGE_ORG}/${IMAGE_NAME}:${ALT_TAG_NAME}"
